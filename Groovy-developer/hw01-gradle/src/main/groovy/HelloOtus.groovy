16616a0ed327c6d9b257419356de2ea17630b74b
kirka0707 <kirka.07.07@gmail.com>
2025-09-04 20:05:33 +0300

    Update HelloOtus.groovy

--- Groovy-developer/hw01-gradle/src/main/groovy/HelloOtus.groovy
+++ Groovy-developer/hw01-gradle/src/main/groovy/HelloOtus.groovy
@@ -1,5 +1,5 @@
 import com.google.common.base.Joiner
 
-def list = ['Hello', 'Otus', 'from', 'Guava']
+def list = ['Hello', 'OTUS', 'from', 'Guava']
 def result = Joiner.on(' ').join(list)
 println result
